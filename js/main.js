716257673a195b153cef4854392682f446408378
sfk345 <sonechka_27_01@mail.ru>
2023-01-30 16:14:31 +0700

    Vue3

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks.",
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "More products like this"
+        link: "More products like this",
+        inStock: true,
+        inventory: 1000,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantID: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantID: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
