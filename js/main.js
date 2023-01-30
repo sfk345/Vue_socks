5e54f99f4d0fd015feed1bc6d24db267c7c5b4eb
sfk345 <sonechka_27_01@mail.ru>
2023-01-30 15:46:18 +0700

    Vue2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks.",
+        image: "./img/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "More products like this"
     }
 })
