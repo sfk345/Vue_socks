04d9dc0865e570f3abe04fd1cab362da61b43629
sfk345 <sonechka_27_01@mail.ru>
2023-01-30 19:04:20 +0700

    Vue6

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks.",
-        image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "More products like this",
-        inStock: false,
-        inventory: 1000,
+        onS: "on Sale",
+        selectedVariant: 0,
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inventory: 101,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantID: 2234,
                 variantColor: 'green',
-                variantIMG: "./img/vmSocks-green-onWhite.jpg"
+                variantIMG: "./img/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantID: 2235,
                 variantColor: 'blue',
-                variantIMG: "./img/vmSocks-blue-onWhite.jpg"
+                variantIMG: "./img/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -31,8 +34,23 @@ let app = new Vue({
         outCart() {
             this.cart -= 1
         },
-        updateProduct(variantIMG){
-            this.image = variantIMG
+        updateProduct(index){
+            this.selectedVariant = index;
+            console.log(index)
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantIMG;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            return this.brand + ' ' + this.product + ' ' + this.onS;
         }
     }
 })
