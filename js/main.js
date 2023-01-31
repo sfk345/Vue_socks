86e61151db42753207825a60c01b85f21391433c
sfk345 <sonechka_27_01@mail.ru>
2023-01-31 11:31:28 +0700

    Vue9

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
+Vue.component('product-review', {
+    template: `
+   <form class="review-form" @submit.prevent="onSubmit">
+   <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+        <li v-for="error in errors">{{error}}</li>
+    </ul>
+   </p>
+   
+   <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+   </p>
+   
+   <p>
+        <label for="review">Review</label>
+        <textarea id="review" v-model="review"></textarea>
+   </p>
+
+   <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+        </select>
+   </p>
+   <p>
+        <label for="recommend">Would you recommend this product?</label>
+        <select id="recommend" v-model.number="recommend">
+            <option>Yes</option>
+            <option>No</option>
+        </select>
+   </p>
+   <p>
+        <input type="submit" value="Submit">
+   </p>
+   </form>
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: [],
+            recommend: null
+        }
+    },
+    methods: {
+        onSubmit(){
+            if(this.name && this.review && this.rating && this.recommend) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if(!this.name) this.errors.push("Name required")
+                if(!this.review) this.errors.push("Review required")
+                if(!this.rating) this.errors.push("Rating required")
+                if(!this.recommend) this.errors.push("Rating recommend")
+            }
+
+        }
+    }
+})
+
+
 Vue.component('product', {
     props: {
         premium: {
@@ -31,9 +108,7 @@ Vue.component('product', {
                 :style="{ backgroundColor:variant.variantColor }"
                 @mouseover="updateProduct(index)">
              </div>
-              
 
-    
              <button
              v-on:click="addToCart"
              :disabled="!inStock"
@@ -46,8 +121,20 @@ Vue.component('product', {
              :class="{ disabledButton: !inStock }">
                 Out of cart
              </button>
-           
         </div>
+        <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{review.name}}</p>
+                        <p>Rating: {{review.rating}}</p>
+                        <p>{{review.review}}</p>
+                        <p>{{review.recommend}}</p>
+                    </li>
+                </ul>
+        </div>
+        <product-review @review-submitted="addReview"></product-review>
     </div>
  `,
     data() {
@@ -76,7 +163,8 @@ Vue.component('product', {
             ],
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-            cart: 0
+            cart: 0,
+            reviews: []
         }
     },
     methods: {
@@ -91,7 +179,11 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
+
     },
     computed: {
         title() {
@@ -137,6 +229,9 @@ let app = new Vue({
         },
     }
 })
+
+
+
 
 
 
